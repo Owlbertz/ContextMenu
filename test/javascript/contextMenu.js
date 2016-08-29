b285084e8dd8e5ee3f81e215ef366e87930ba4ea
Marius Olbertz <marius.olbertz@gmail.com>
2016-08-29 17:47:16 +0200

    Enhanced automated test cases.

--- test/javascript/contextMenu.js
+++ test/javascript/contextMenu.js
@@ -7,9 +7,9 @@ describe('ContextMenu', function() {
     + '<li data-context-menu="#menu">Item 3</li>'
   + '</ul>'
   +'<ul class="context menu dropdown vertical" role="menu" id="menu">'
-    + '<li role="menuitem"><a href="#">Copy</a></li>'
-    + '<li role="menuitem"><a href="#">Move</a></li>'
-    + '<li role="menuitem"><a href="#">Delete</a></li>'
+    + '<li role="menuitem"><a href>Copy</a></li>'
+    + '<li role="menuitem"><a href>Move</a></li>'
+    + '<li role="menuitem"><a href>Delete</a></li>'
   + '</ul>';
 
   afterEach(function() {
@@ -33,6 +33,48 @@ describe('ContextMenu', function() {
       plugin = new Foundation.ContextMenu($html.find('[data-context-menu]'), {});
 
       plugin.$element.should.have.attr('data-context-menu');
+      plugin.$menu.should.be.hidden;
+    });
+  });
+
+
+  describe('show()', function() {
+    it('shows the menu', function() {
+      $html = $(templateHtml).appendTo('body');
+      plugin = new Foundation.ContextMenu($html.find('[data-context-menu]'), {});
+
+      plugin.show();
+      plugin.$menu.should.be.visible;
+    });
+  });
+
+  describe('hide()', function() {
+    it('hides the menu', function() {
+      $html = $(templateHtml).appendTo('body');
+      plugin = new Foundation.ContextMenu($html.find('[data-context-menu]'), {});
+
+      plugin.show();
+      plugin.hide();
+      plugin.$menu.should.be.hidden;
+    });
+  });
+
+  describe('events', function() {
+    /*it('shows menu on touchstart', function() {
+      $html = $(templateHtml).appendTo('body');
+      plugin = new Foundation.ContextMenu($html.find('[data-context-menu]'), {});
+
+      plugin.$element.trigger('touchstart');
+      plugin.$menu.should.be.visible;
+    });*/
+
+    it('hides menu on body click', function() {
+      $html = $(templateHtml).appendTo('body');
+      plugin = new Foundation.ContextMenu($html.find('[data-context-menu]'), {});
+
+      plugin.show();
+
+      $('body').trigger('click');
       plugin.$menu.should.be.hidden;
     });
   });
